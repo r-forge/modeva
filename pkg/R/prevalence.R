3e0a011eda97d74ef4686281d9ec24d95ee99dbf
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2021-09-30 11:59:22 +0000

    prevalence now accepts a model object as input
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/prevalence.R
+++ pkg/R/prevalence.R
@@ -1,9 +1,29 @@
 prevalence <-
-function (obs, event = 1, na.rm = TRUE) {
-  # version 1.0
-  # calculates the prevalence (proportion of occurrences) of a value (event) in a vector
-  # obs: a vector of binary observations (e.g. 1 vs. 0, male vs. female, disease vs. no disease, etc.)
-  # event: the value whose prevalence we want to calculate (e.g. 1, "present", etc.)
+function (obs = NULL, model = NULL, event = 1, na.rm = TRUE) {
+  # version 1.1 (30 sep 2021)
+
+  #model_classes_implemented <- c("glm", "gam", "gbm", "randomForest", "bart")
+  #if (any(class(obs) %in% model_classes_implemented))  model <- obs  # (in case the user provides unnamed model argument)
+  
+  if (!is.null(obs)) {
+    if (!is(obs, "vector")) stop("'obs' must be a vector.")
+    if (!(event %in% obs)) stop("'event' is not among of the values of 'obs'.")
+  }
+  
+  if (!is.null(model)) {
+    if (!is.null(obs)) warning("Argument 'obs' ignored in favour of 'model'.")
+    if (is(model, "glm") || is(model, "gam")) {
+      obs <- model$y
+    } else if (is(model, "gbm")) {
+      obs <- model$data$y
+    } else if (is(model, "randomForest")) {
+      obs <- model$y
+    } else if (is(model, "bart")) {
+      if (is.null(model$fit$data)) stop("'$fit$data' section not available in 'model'. Try computing the 'bart' model with 'keeptrees=TRUE', or providing 'obs' here instead of 'model'.")
+      obs <- model$fit$data@y  # requires model ran with keeptrees=TRUE
+    } else stop("'model' is of a non-implemented class.")
+  }  # end if model
+  
   if(na.rm) obs <- obs[!is.na(obs)]
   sum(obs == event) / length(obs)
 }
