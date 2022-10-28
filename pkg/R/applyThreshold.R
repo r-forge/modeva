0be1973564d4319f4b4fd59f859a9f3d024485b8
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2022-10-28 12:26:17 +0000

    fixed applyThreshold bug sometimes thresh came out character
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@172 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/applyThreshold.R
+++ pkg/R/applyThreshold.R
@@ -1,28 +1,28 @@
 applyThreshold <- function(model = NULL, obs = NULL, pred = NULL, thresh, right = FALSE, interval = 0.01, quant = 0, na.rm = TRUE) {
-  
-  # version 1.1 (5 Jul 2022)
-  
+
+  # version 1.2 (28 Oct 2022)
+
   if(!(length(thresh) %in% 1:2)) stop ("'thresh' must be of length 1 or 2.")
   if (!(is.numeric(thresh) || all(thresh %in% modEvAmethods("getThreshold"))))
     stop("'thresh' must be EITHER numeric OR among the options obtained with modEvAmethods('getThreshold')")
-  
+
   pred_in <- pred  # in case input is raster, so final reclass is also raster
-  
+
   obspred <- inputMunch(model, obs, pred)
   if (!is.null(obs) || !is.null(model)) obs <- obspred[ , "obs"]
   pred <- obspred[ , "pred"]
-  
+
   if (any(thresh %in% modEvAmethods("getThreshold"))) {
     for (i in which(thresh %in% modEvAmethods("getThreshold"))) {
       thresh[i] <- getThreshold(obs = obs, pred = pred, threshMethod = thresh[i], interval = interval, quant = quant, na.rm = na.rm)
     }
   }
-  
-  thresh <- sort(thresh)
-  
+
+  thresh <- sort(as.numeric(thresh))
+
   if (inherits(pred_in, "SpatRaster")) reclass <- pred_in
   else reclass <- pred
-  
+
   reclass[reclass < thresh[1]] <- 0
   reclass[reclass > thresh[length(thresh)]] <- 1
   if (length(thresh) == 2)  reclass[reclass > thresh[1] & reclass < thresh[2]] <- 0.5
@@ -34,6 +34,6 @@ applyThreshold <- function(model = NULL, obs = NULL, pred = NULL, thresh, right 
     reclass[reclass == thresh[length(thresh)]] <- 1
     if (length(thresh) == 2)  reclass[reclass == thresh[2]] <- 0.5
   }
-  
+
   return(reclass)
 }
