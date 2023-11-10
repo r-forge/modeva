a3e796eade73f641a42e8dfcd951ebcc687edb5b
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2023-11-10 13:30:15 +0000

    added similarity and quantReclass functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@190 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/modEvAmethods.R
+++ pkg/R/modEvAmethods.R
@@ -1,22 +1,24 @@
 modEvAmethods <- function(fun) {
   # version 1.4 (11 Apr 2022)
-  
+
   if (fun %in% c("threshMeasures", "multModEv")) {
     thresh.measures <- c("CCR", "Misclass", "Sensitivity", "Specificity", "Omission", "Commission", "Precision", "Recall", "PPP", "NPP", "UPR", "OPR", "PPI", "PAI", "kappa", "TSS", "NMI", "F1score", "OddsRatio")
 
     if (fun == "threshMeasures") return(thresh.measures)
-    
+
     else if (fun == "multModEv") {
       bin.measures <- c("HL", "HL.p", "RMSE")  # "ABCc", "rABCc", "unityRsq"
       return (c("Prevalence", "AUC", "MeanPrecision", "AUCPR", thresh.measures, bin.measures, "Miller.int", "Miller.slope"))  # "Evenness", "ABCc", "rABCc", "unityRsq", "Miller.p"
     }  # end if multModEv
   }  # end if threshMeasures | multModEv
-  
+
+  else if (fun == "similarity") return(c("Jaccard", "Sorensen"))
+
   else if (fun == "getBins") return(c("round.prob", "prob.bins", "size.bins", "n.bins", "quantiles"))
-  
+
   else if (fun == "optiThresh") return(c("each", "preval", "0.5", "maxKappa", "minSensSpecDiff", "maxSensSpecSum", "maxTSS"))
-  
+
   else if (fun == "getThreshold") return(sort(c("preval", "trainPrev", "meanPred", "midPoint", "maxKappa", "maxCCR", "maxOPS", "maxOA", "maxF", "maxSSS", "maxTSS", "minDSS", "minDPR", "maxSPR", "minD01", "minD11", "equalPrev", "MTP")))
-  
+
   else stop ("Invalid 'fun' argument. Check (case-sensitive) spelling.")
 }
