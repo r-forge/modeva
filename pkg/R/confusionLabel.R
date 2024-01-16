5cbe7b5fd87ee90942c482ae3c1f640487e2b68f
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2024-01-16 18:14:35 +0000

    confusionLabel output categorical SpatRast
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@196 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/confusionLabel.R
+++ pkg/R/confusionLabel.R
@@ -1,5 +1,5 @@
 confusionLabel <- function(model = NULL, obs = NULL, pred = NULL, thresh, interval = 0.01, quant = 0, verbosity = 2, na.rm = FALSE, rm.dup = FALSE) {
-  # version 1.7 (14 Jun 2022)
+  # version 1.8 (16 Jan 2024)
 
   pred_in <- pred  # in case input is raster, so final reclass is also raster
 
@@ -7,22 +7,23 @@ confusionLabel <- function(model = NULL, obs = NULL, pred = NULL, thresh, interv
   obs <- obspred[ , "obs"]
   pred <- obspred[ , "pred"]
 
-  if (!(is.numeric(thresh) || thresh %in% modEvAmethods("getThreshold")))
-    stop("'thresh' must be either a numeric value between 0 and 1, or one of the options obtained with modEvAmethods('getThreshold')")
+  if (!(is.numeric(thresh) || thresh %in% modEvAmethods("getThreshold")))  stop("'thresh' must be either a numeric value between 0 and 1, or one of the options obtained with modEvAmethods('getThreshold')")
   if (thresh %in% modEvAmethods("getThreshold"))  thresh <- getThreshold(obs = obs, pred = pred, threshMethod = thresh, interval = interval, quant = quant, na.rm = na.rm)
 
-  res <- rep("", length(obs))
-  res[pred >= thresh & obs == 1] <- "TruePos"
-  res[pred < thresh & obs == 0] <- "TrueNeg"
-  res[pred >= thresh & obs == 0] <- "FalsePos"
-  res[pred < thresh & obs == 1] <- "FalseNeg"
+  output <- rep("", length(obs))
+  output[pred >= thresh & obs == 1] <- "TruePos"
+  output[pred < thresh & obs == 0] <- "TrueNeg"
+  output[pred >= thresh & obs == 0] <- "FalsePos"
+  output[pred < thresh & obs == 1] <- "FalseNeg"
 
   if (inherits(pred_in, "SpatRaster")) {
-    finite_pixels <- is.finite(terra::values(pred_in))
-    terra::values(pred_in)[finite_pixels] <- as.integer(as.factor(res))
-    res <- pred_in
-    # levels(res) <-
+    finite_pixels <- which(is.finite(terra::values(pred_in)))
+    output_rast <- pred_in
+    levs <- as.factor(output)
+    terra::values(output_rast)[finite_pixels] <- levs  # only 'finite_pixels' because output is shorter when input has NAs
+    levels(output_rast) <- data.frame(id = unique(as.integer(levs)), cat = unique(output))
+    output <- output_rast
   }
 
-  res
+  output
 }
