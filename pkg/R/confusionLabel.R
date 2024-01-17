2fb5835f038f2a04dabf00ce91cabbdccf721598
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2024-01-17 11:46:43 +0000

    confusionLabel colr table for out rast; plot=T arg
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@197 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/confusionLabel.R
+++ pkg/R/confusionLabel.R
@@ -1,5 +1,5 @@
-confusionLabel <- function(model = NULL, obs = NULL, pred = NULL, thresh, interval = 0.01, quant = 0, verbosity = 2, na.rm = FALSE, rm.dup = FALSE) {
-  # version 1.8 (16 Jan 2024)
+confusionLabel <- function(model = NULL, obs = NULL, pred = NULL, thresh, interval = 0.01, quant = 0, verbosity = 2, na.rm = FALSE, rm.dup = FALSE, plot = TRUE) {
+  # version 1.9 (17 Jan 2024)
 
   pred_in <- pred  # in case input is raster, so final reclass is also raster
 
@@ -10,20 +10,32 @@ confusionLabel <- function(model = NULL, obs = NULL, pred = NULL, thresh, interv
   if (!(is.numeric(thresh) || thresh %in% modEvAmethods("getThreshold")))  stop("'thresh' must be either a numeric value between 0 and 1, or one of the options obtained with modEvAmethods('getThreshold')")
   if (thresh %in% modEvAmethods("getThreshold"))  thresh <- getThreshold(obs = obs, pred = pred, threshMethod = thresh, interval = interval, quant = quant, na.rm = na.rm)
 
-  output <- rep("", length(obs))
-  output[pred >= thresh & obs == 1] <- "TruePos"
-  output[pred < thresh & obs == 0] <- "TrueNeg"
-  output[pred >= thresh & obs == 0] <- "FalsePos"
-  output[pred < thresh & obs == 1] <- "FalseNeg"
+  out_chr <- rep("", length(obs))
+  out_chr[pred >= thresh & obs == 1] <- "TruePos"
+  out_chr[pred < thresh & obs == 0] <- "TrueNeg"
+  out_chr[pred >= thresh & obs == 0] <- "FalsePos"
+  out_chr[pred < thresh & obs == 1] <- "FalseNeg"
 
   if (inherits(pred_in, "SpatRaster")) {
     finite_pixels <- which(is.finite(terra::values(pred_in)))
-    output_rast <- pred_in
-    levs <- as.factor(output)
-    terra::values(output_rast)[finite_pixels] <- levs  # only 'finite_pixels' because output is shorter when input has NAs
-    levels(output_rast) <- data.frame(id = unique(as.integer(levs)), cat = unique(output))
-    output <- output_rast
+    out_rast <- pred_in
+    levs <- as.factor(out_chr)
+    terra::values(out_rast)[finite_pixels] <- levs  # only 'finite_pixels' because out_chr vector is shorter when input has NAs
+
+    # convert to categorical raster:
+    levels(out_rast) <- data.frame(id = as.integer(unique(levs)), cat = unique(out_chr))
+
+    # set colours for raster categories:
+    colr_table <- data.frame(lev = as.factor(c("TruePos", "FalsePos", "TrueNeg", "FalseNeg")), col = c("blue", "lightblue", "red", "orange"))
+    existing_levs <- colr_table$lev %in% levels(out_rast)[[1]]$cat
+    terra::coltab(out_rast) <- data.frame(
+      values = droplevels(colr_table$lev[existing_levs]),
+      cols = colr_table$col[existing_levs])
+
+    if (plot) terra::plot(out_rast, mar = c(2, 2, 2, 5))
+
+    return(out_rast)
   }
 
-  output
+  return(out_chr)
 }
