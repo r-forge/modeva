18004228dc5cfe532559af7dc86463c9bb7e7253
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2024-06-06 09:35:59 +0000

    varImp relative optional for BARTs; flexBART reorder update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@206 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/varImp.R
+++ pkg/R/varImp.R
@@ -1,6 +1,6 @@
-varImp <- function(model, imp.type = "each", relative = TRUE, reorder = TRUE, group.cats = FALSE, plot = TRUE, plot.type = "lollipop", error.bars = "sd", ylim = "auto", col = c("#4477aa", "#ee6677"), plot.points = TRUE, legend = TRUE, grid = TRUE, ...) {
+varImp <- function(model, imp.type = "each", relative = TRUE, reorder = TRUE, group.cats = FALSE, plot = TRUE, plot.type = "lollipop", error.bars = "sd", ylim = "auto", col = c("#4477aa", "#ee6677"), plot.points = TRUE, legend = TRUE, grid = TRUE, verbosity = 2, ...) {
 
-  # version 2.1 (5 Jun 2024)
+  # version 2.2 (6 Jun 2024)
 
   # if 'col' has length 2 and varImp has negative values (e.g. for z-value), those will get the second colour
 
@@ -20,9 +20,9 @@ varImp <- function(model, imp.type = "each", relative = TRUE, reorder = TRUE, gr
 
   if (!is_bart && !is_flexbart)  error.bars <- NA
 
-  if (reorder && is_flexbart) {
+  if (reorder && is_flexbart && is.null(colnames(model[["varcounts"]]))) {
     reorder <- FALSE
-    message ("'reorder' set to FALSE, as 'flexBART' does not currently carry variable names, which would make it impossible to match variables with importance values. Variables are in the order in which they were provided to the 'flexBART' model, with the continuous preceding the categorical ones.")
+    if (verbosity > 0) message ("'reorder' set to FALSE, as this version of 'flexBART' does not carry variable names, which would make it impossible to match variables with importance values. Variables are in the order in which they were provided to the 'flexBART' model, with the continuous preceding the categorical ones. Update 'flexBART' if you want the variables named and reordered.")
   }
 
   if (is(model, "glm")) {  #  && !is(model, "Gam")
@@ -31,7 +31,7 @@ varImp <- function(model, imp.type = "each", relative = TRUE, reorder = TRUE, gr
 
     # if (measure == "z") {
       metric <- ifelse(isTRUE(relative), "Relative z value", "Absolute z value")
-      cat("\nMetric:", metric, "\n\n")
+      if (verbosity > 1) cat("\nMetric:", metric, "\n\n")
       varimp <- summary(model)$coefficients[-1, "z value"]
       if (isTRUE(relative)) varimp <- varimp / sum(abs(varimp))
       ylab <- metric
@@ -51,7 +51,7 @@ varImp <- function(model, imp.type = "each", relative = TRUE, reorder = TRUE, gr
     # requireNamespace("gbm")  # would require a suggest/depend
     if ("gbm" %in% .packages()) {
       metric <- "Relative influence"
-      cat("\nMetric:", metric, "\n\n")
+      if (verbosity > 1) cat("\nMetric:", metric, "\n\n")
       ylab <- metric
       smry <- summary(model, plotit = FALSE)
       varimp <- smry[ , "rel.inf"] / 100
@@ -63,7 +63,7 @@ varImp <- function(model, imp.type = "each", relative = TRUE, reorder = TRUE, gr
 
   else if (is(model, "randomForest")) {
     metric <- colnames(model$importance)
-    cat("\nMetric:", metric, "\n\n")
+    if (verbosity > 1) cat("\nMetric:", metric, "\n\n")
     varimp <- model$importance  # / nrow(model$importance) / 100  # doesn't work well for mean accuracy decrease
     names(varimp) <- rownames(model$importance)
     ylab <- metric
@@ -72,16 +72,17 @@ varImp <- function(model, imp.type = "each", relative = TRUE, reorder = TRUE, gr
 
   else if (is_bart || is_flexbart) {
 
-    metric <- "Proportion of splits used"
-    # metric <- ifelse(isTRUE(relative), "Proportion of splits used", "Number of splits used")
-    cat("\nMetric:", metric, "\n\n")
+    # metric <- "Proportion of splits used"
+    metric <- ifelse(isTRUE(relative), "Proportion of splits used", "Number of splits used")
+    if (verbosity > 1) cat("\nMetric:", metric, "\n\n")
     ylab <- metric
 
     if ("varcounts" %in% names(model))  # in flexBART models
       names(model)[grep("varcounts", names(model))] <- "varcount"  # to homogenize
 
-    varimps <- model[["varcount"]] / rowSums(model[["varcount"]])
-
+    # varimps <- model[["varcount"]] / rowSums(model[["varcount"]])
+    varimps <- model[["varcount"]]
+    if (isTRUE(relative)) varimps <- varimps / rowSums(model[["varcount"]])
     varimp <- colMeans(varimps)
 
     if (group.cats) {
@@ -121,7 +122,7 @@ varImp <- function(model, imp.type = "each", relative = TRUE, reorder = TRUE, gr
 
       n.dropped <- length(dropped.vars)
       if (n.dropped > 0) {
-        message("The following variables had been automatically dropped by the model (e.g. for having no variability):  ", paste(dropped.vars, collapse = ", "))
+        if (verbosity > 0) message("The following variables had been automatically dropped by the model (e.g. for having no variability):  ", paste(dropped.vars, collapse = ", "))
         dropped.varimp <- rep(0, n.dropped)
         names(dropped.varimp) <- dropped.vars
         varimp <- c(varimp, dropped.varimp)
