69cd083154c2eebe36ca067dfbaef8a934e6309b
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2024-10-02 17:55:02 +0000

    add errorMeasures(); update quantReclass ref
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/modeva@218 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/errorMeasures.R
@@ -0,0 +1,15 @@
+errorMeasures <- function(model = NULL, obs = NULL, pred = NULL, na.rm = TRUE, rm.dup = FALSE) {
+  # version 1.0 (2 Oct 2024)
+
+  obspred <- inputMunch(model, obs, pred, na.rm = na.rm, rm.dup = rm.dup)
+  obs <- obspred[ , "obs"]
+  pred <- obspred[ , "pred"]
+
+  SE <- (obs - pred) ^ 2
+  MSE <- mean(SE)
+  Brier_score <- MSE
+  RMSE <- sqrt(MSE)
+  # SSE <- sum(SE)
+
+  return(list(RMSE = RMSE, MSE = MSE, Brier_score = Brier_score))
+}
